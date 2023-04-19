2be3fcdd54732c977078606973d4f03e7f3a2b91
vampyar <yaromir92@gmail.com>
2023-04-19 18:43:59 +0200

    Fix lint deploy
    Signed-off-by: vampyar <yaromir92@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,36 @@
-// import News, { INewsProps } from '@/components/news/news';
-// import { getBaseUrl } from '@/utils/api';
-// import { TNewsResponse } from '@/pages/api/news';
+import News, { INewsProps } from '@/components/news/news';
+import { getBaseUrl } from '@/utils/api';
+import { TNewsResponse } from '@/pages/api/news';
 
-export default function Home() {
-  return <div className="flex">{/*<News {...props} />*/}</div>;
+export default function Home(props: INewsProps) {
+  return (
+    <>
+      <div className="flex">
+        <News {...props} />
+      </div>
+    </>
+  );
 }
 
-// export async function getStaticProps() {
-//   const res = await fetch(getBaseUrl() + '/api/news');
-//   const news: TNewsResponse = await res.json();
-//   if (news.message) {
-//     return {
-//       props: {
-//         news: [],
-//         message: news.message,
-//       },
-//     };
-//   }
-//
-//   return {
-//     props: {
-//       news,
-//     },
-//     // Next.js will attempt to re-generate the page:
-//     // - When a request comes in
-//     // - At most once every 10 seconds
-//     revalidate: 10, // In seconds
-//   };
-// }
+export async function getStaticProps() {
+  const res = await fetch(getBaseUrl() + '/api/news');
+  const news: TNewsResponse = await res.json();
+  if (news?.message) {
+    return {
+      props: {
+        news: [],
+        message: news.message,
+      },
+    };
+  }
+
+  return {
+    props: {
+      news: news || [],
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  };
+}
